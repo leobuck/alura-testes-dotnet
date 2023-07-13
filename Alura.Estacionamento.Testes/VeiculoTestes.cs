a8ff59f44447c2522d996f16eb2821bc7617bf91
leobuck <leobuck12@gmail.com>
2023-07-12 21:46:32 -0300

    Testes unitários e o padrão AAA

--- Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -6,11 +6,18 @@ namespace Alura.Estacionamento.Testes
 {
     public class VeiculoTestes
     {
+        // Arrange: Preparacao do cenario
+        // Act: Metodo que sera testado
+        // Assert: Verificacao do resultado obtido
+
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            // Arrange
             var veiculo = new Veiculo();
+            // Act
             veiculo.Acelerar(10);
+            // Assert
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
